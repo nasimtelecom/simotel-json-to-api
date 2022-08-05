8fc52fb36e6b5dc6ea090ab36b1181e7fecdb32a
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2022-08-05 13:04:52 +0430

    Update MainController.php

--- app/Controller/MainController.php
+++ app/Controller/MainController.php
@@ -4,7 +4,47 @@ namespace App\Controller;
 
 class MainController
 {
-    public function index(){
-        die ("Test Ok!");
+    public function index()
+    {
+        $jsonFile = __DIR__ . "/../../simotel-json-api/simotel.json";
+        $jsonContent = json_decode(file_get_contents($jsonFile), true);
+
+        $apiArray = $this->jsonToSimotelApiArray($jsonContent);
+
+        $data = varexport($apiArray, true);
+
+        try {
+            $this->exportToFile($data);
+            echo "simotel api list maped to export/simotelApiList.php";
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+
+    private function jsonToSimotelApiArray($json)
+    {
+        $items = $json["item"];
+        $api=[];
+        foreach ($items as $item) {
+            $capi=[];
+            $capi["address"]=$item["name"];
+            $capi["method"]=$item["request"]["method"];
+            $name = str_replace("/", "_", $item["name"]);
+            $api[$name]=$capi;
+        }
+        return $api;
+    }
+
+    private function exportToFile($content)
+    {
+        if (!is_dir(__DIR__ . "/../../export")) {
+            mkdir(__DIR__ . "/../../export");
+        }
+
+        $exportFileName = __DIR__ . "/../../export/simotelApiList.php";
+        file_put_contents($exportFileName, "<?php \r\n return ".$content.";");
+
+        return true;
     }
 }
