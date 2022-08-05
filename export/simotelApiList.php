36095a82ea47c35077af71e1af97abb8fae34925
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2022-08-05 13:14:32 +0430

    Update simotelApiList.php

--- export/simotelApiList.php
+++ export/simotelApiList.php
@@ -1,477 +1,477 @@
 <?php 
  return [
   'pbx_users_add' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/users/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_users_update' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/users/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_users_remove' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/users/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_users_search' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/users/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_trunks_add' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/trunks/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_trunks_update' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/trunks/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_trunks_remove' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/trunks/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_trunks_search' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/trunks/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_add' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_update' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_remove' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_search' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_addagent' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/addagent',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_removeagent' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/removeagent',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_pauseagent' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/pauseagent',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_resumeagent' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/resumeagent',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_batchaddagent' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/batchaddagent',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_batchremoveagent' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/batchremoveagent',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_batchpauseagent' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/batchpauseagent',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_queues_batchresumeagent' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/queues/batchresumeagent',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_blacklists_add' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/blacklists/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_blacklists_update' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/blacklists/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_blacklists_remove' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/blacklists/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_blacklists_search' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/blacklists/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_whitelists_add' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/whitelists/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_whitelists_update' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/whitelists/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_whitelists_remove' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/whitelists/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_whitelists_search' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/whitelists/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_announcements_upload' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/announcements/upload',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_announcements_add' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/announcements/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_announcements_update' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/announcements/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_announcements_remove' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/announcements/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_announcements_search' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/announcements/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_musiconholds_search' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/musiconholds/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_faxes_upload' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/faxes/upload',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_faxes_add' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/faxes/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_faxes_search' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/faxes/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'pbx_faxes_download' => [
-    'default_data' => [
-    ],
     'address' => 'pbx/faxes/download',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'call_originate_act' => [
-    'default_data' => [
-    ],
     'address' => 'call/originate/act',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'voicemails_voicemails_add' => [
-    'default_data' => [
-    ],
     'address' => 'voicemails/voicemails/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'voicemails_voicemails_update' => [
-    'default_data' => [
-    ],
     'address' => 'voicemails/voicemails/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'voicemails_voicemails_remove' => [
-    'default_data' => [
-    ],
     'address' => 'voicemails/voicemails/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'voicemails_voicemails_search' => [
-    'default_data' => [
-    ],
     'address' => 'voicemails/voicemails/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'voicemails_audio_download' => [
-    'default_data' => [
-    ],
     'address' => 'voicemails/audio/download',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'voicemails_inbox_search' => [
-    'default_data' => [
-    ],
     'address' => 'voicemails/inbox/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_quick_search' => [
-    'default_data' => [
-    ],
     'address' => 'reports/quick/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_quick_info' => [
-    'default_data' => [
-    ],
     'address' => 'reports/quick/info',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_audio_download' => [
-    'default_data' => [
-    ],
     'address' => 'reports/audio/download',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_cdr_search' => [
-    'default_data' => [
-    ],
     'address' => 'reports/cdr/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_cdr_ordered_id' => [
-    'default_data' => [
-    ],
     'address' => 'reports/cdr/ordered/id',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_queue_search' => [
-    'default_data' => [
-    ],
     'address' => 'reports/queue/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_queue_ordered_id' => [
-    'default_data' => [
-    ],
     'address' => 'reports/queue/ordered/id',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_queue_details_search' => [
-    'default_data' => [
-    ],
     'address' => 'reports/queue_details/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_queue_details_ordered_id' => [
-    'default_data' => [
-    ],
     'address' => 'reports/queue_details/ordered/id',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_agent_search' => [
-    'default_data' => [
-    ],
     'address' => 'reports/agent/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'reports_poll_search' => [
-    'default_data' => [
-    ],
     'address' => 'reports/poll/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_announcements_upload' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/announcements/upload',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_announcements_add' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/announcements/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_announcements_update' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/announcements/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_announcements_remove' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/announcements/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_announcements_search' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/announcements/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_campaigns_add' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/campaigns/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_campaigns_update' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/campaigns/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_campaigns_remove' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/campaigns/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_campaigns_search' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/campaigns/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_contacts_add' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/contacts/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_contacts_update' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/contacts/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_contacts_remove' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/contacts/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_contacts_search' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/contacts/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_groups_upload' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/groups/upload',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_groups_add' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/groups/add',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_groups_update' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/groups/update',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_groups_remove' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/groups/remove',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_groups_search' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/groups/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_reports_search' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/reports/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_reports_ordered_id' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/reports/ordered/id',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_reports_info' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/reports/info',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
   'autodialer_trunks_update' => [
-    'default_data' => [
-    ],
     'address' => 'autodialer/trunks/update',
     'method' => 'POST',
-  ],
-  'autodialer_trunks_search' => [
     'default_data' => [
     ],
+  ],
+  'autodialer_trunks_search' => [
     'address' => 'autodialer/trunks/search',
     'method' => 'POST',
+    'default_data' => [
+    ],
   ],
 ];
